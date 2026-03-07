a88c7cc94228bd2d99632b14998cf04414b28460
Анаит <@email.com>
2026-03-07 22:18:28 +0300

    fix port conflict

--- app.py
+++ app.py
@@ -55,6 +55,4 @@ def analyze():
         return jsonify({"success": False, "error": str(e)})
 
 if __name__ == '__main__':
-    import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+    app.run()
